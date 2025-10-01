db43203ca608fb683d77a678c8c5df1a173217ee
Jäger <w.s.jaeger@vu.nl>
2025-10-01 21:11:26 +0200

    small adjustments to read gdis

--- example_validation.py
+++ example_validation.py
@@ -9,16 +9,16 @@ from validation.io import load_geocoded_batch, load_emdat_archive
 
 # Define logger to track the validation process
 logging.basicConfig(
-    filename='example_validation.log',
+    filename="example_validation.log",
     level=logging.INFO,
-    filemode='a',
-    style='{',
-    format='{asctime} - {levelname} - {message}',
-    datefmt="%Y-%m-%d %H:%M:%S"
+    filemode="a",
+    style="{",
+    format="{asctime} - {levelname} - {message}",
+    datefmt="%Y-%m-%d %H:%M:%S",
 )
 
 # Params
-GEOMTYPE = 'gadm'
+GEOMTYPE = "gadm"  # "gdis"
 BATCH_START = 2000
 BATCH_END = 2002
 
@@ -31,13 +31,18 @@ DISSOLVE_UNITS = False
 # Method for calculating areas
 # If 'geodetic', calculate areas using WGS84 ellipsoid
 # If 'equal_area', reproject to equal-area CRS and calculate planar areas
-AREA_CALCULATION_METHOD: Literal["geodetic", "equal_area"] = 'geodetic'
+AREA_CALCULATION_METHOD: Literal["geodetic", "equal_area"] = "geodetic"
 
-GEOCODED_BATCH_FILE = Path(f'data/geoloc_emdat_0002_{GEOMTYPE}.gpkg')
-EMDAT_BATCH_FILE = Path(f'data/geoemdat_{BATCH_START}_{BATCH_END}.gpkg')
-EMDAT_ARCHIVE_FILE = Path('data/241204_emdat_archive.xlsx')
-OUTPUT_FILENAME = (f"validation_{GEOMTYPE}_{BATCH_START}_{BATCH_END}"
-                   f"{'_dissolved' if DISSOLVE_UNITS else ''}.csv")
+# GAUL or GDIS validation file
+GEOCODED_BATCH_FILE = Path(f"data/geoloc_emdat_0002_{GEOMTYPE}.gpkg")
+# LLM locations file
+EMDAT_BATCH_FILE = Path(f"data/geoemdat_{BATCH_START}_{BATCH_END}.gpkg")
+# EM-DAT tabular file
+EMDAT_ARCHIVE_FILE = Path("data/241204_emdat_archive.xlsx")
+OUTPUT_FILENAME = (
+    f"validation_{GEOMTYPE}_{BATCH_START}_{BATCH_END}"
+    f"{'_dissolved' if DISSOLVE_UNITS else ''}.csv"
+)
 OUTPUT_COLUMNS = [
     "dis_no",
     "name",
@@ -52,7 +57,7 @@ OUTPUT_COLUMNS = [
     "union_area",
     "a_in_b",
     "b_in_a",
-    "jaccard"
+    "jaccard",
 ]
 
 # Official disaster ids from the Archive
@@ -61,9 +66,10 @@ DISNO_OFFICIAL = load_emdat_archive(
     file_path=EMDAT_ARCHIVE_FILE,
     min_year=BATCH_START,
     max_year=BATCH_END,
-    use_columns=['DisNo.'],
-    geocoded_only=True
-)['DisNo.'].to_list()
+    use_columns=["DisNo."],
+    geocoded_only=True,
+)["DisNo."].to_list()
+
 
 def main():
     logging.info(f"Starting {GEOMTYPE} validation".upper())
@@ -77,13 +83,8 @@ def main():
     if DISSOLVE_UNITS:
         logging.info("Dissolving units")
         geocoded_gdf = geocoded_gdf.dissolve(
-            by='DisNo.',
-            aggfunc={
-                'name': list,
-                'admin_level': list,
-                'admin1': list,
-                'admin2': list
-            }
+            by="DisNo.",
+            aggfunc={"name": list, "admin_level": list, "admin1": list, "admin2": list},
         )
         geocoded_gdf.reset_index(inplace=True)
         logging.info(f"{len(geocoded_gdf)} records after dissolving")
@@ -91,7 +92,7 @@ def main():
     # 3. Filter based on official disaster ids
     logging.info(f"Filtering based on Dis No.")
     # Excluding unpublished and non-geocoded disasters
-    geocoded_gdf = geocoded_gdf[geocoded_gdf['DisNo.'].isin(DISNO_OFFICIAL)]
+    geocoded_gdf = geocoded_gdf[geocoded_gdf["DisNo."].isin(DISNO_OFFICIAL)]
     logging.info(f"{len(geocoded_gdf)} records filtered based on Dis No.")
 
     # 4. Load EMDAT geocoded batch
@@ -106,16 +107,16 @@ def main():
     result_df = pd.DataFrame(columns=OUTPUT_COLUMNS)
 
     for ix, row in geocoded_gdf.iterrows():
-        disno = row['DisNo.']
-        geom_a = row['geometry']
-        geom_b = emdat_gdf[emdat_gdf['disno_'] == disno]['geometry'].iloc[0]
+        disno = row["DisNo."]
+        geom_a = row["geometry"]
+        geom_b = emdat_gdf[emdat_gdf["disno_"] == disno]["geometry"].iloc[0]
         indices: dict[str, float] = calculate_area_indices(geom_a, geom_b)
         results = [
-            row['DisNo.'],
-            row['name'],
-            row['admin_level'],
-            row['admin1'],
-            row['admin2'],
+            row["DisNo."],
+            row["name"],
+            row["admin_level"],
+            row["admin1"],
+            row["admin2"],
             GEOMTYPE,
             AREA_CALCULATION_METHOD,
         ] + list(indices.values())
@@ -123,6 +124,7 @@ def main():
     logging.info(f"Saving results to {OUTPUT_FILENAME}")
     result_df.to_csv(OUTPUT_FILENAME, index=False)
     logging.info(f"Validation complete".upper())
+
 
 if __name__ == "__main__":
     main()
